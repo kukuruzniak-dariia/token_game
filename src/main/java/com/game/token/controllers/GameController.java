cff69448ac8ef1432d0f25fbf4344f9d64451447
Dariia Kukuruzniak <dariia_kukuruzniak@epam.com>
2021-01-14 19:12:19 +0200

    Implement API GET '/game/start'

--- src/main/java/com/game/token/controllers/GameController.java
+++ src/main/java/com/game/token/controllers/GameController.java
@@ -1,17 +1,23 @@
 package com.game.token.controllers;
 
 import com.game.token.entities.Game;
+import com.game.token.services.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping(value = "/game")
 public class GameController {
+    @Autowired
+    private GameService gameService;
 
-@GetMapping("/start")
+    @GetMapping("/start")
     public ResponseEntity<Game> start() {
-        return null;
+        Game game = gameService.start();
+        return new ResponseEntity<>(game, HttpStatus.OK);
     }
 }
--- src/main/java/com/game/token/services/GameService.java
+++ src/main/java/com/game/token/services/GameService.java
@@ -1,7 +1,9 @@
 package com.game.token.services;
 
 import com.game.token.entities.Game;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface GameService {
     int INITIAL_TOKEN_POSITION = 1;
     int LAST_TOKEN_POSITION = 100;
